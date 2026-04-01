c9293cfe1cbedf5dac51744ad3c32b83932066c9
Akashsingh86500 <akchaudhary86500@gmail.com>
2026-04-01 14:19:53 +0530

    [Akash_Singh] Added product schema for MongoDB with Mongoose

---
+++ product.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: String,
+    category: String,
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    image: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
